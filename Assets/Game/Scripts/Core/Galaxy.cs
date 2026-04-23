a82c070cb04a6a485b6fcd99391eaaf73a1f6568
GUOZIJIAN <guozijian8848@163.com>
2026-04-23 23:50:57 +0800

    4.23

--- Assets/Game/Scripts/Core/Galaxy.cs
+++ Assets/Game/Scripts/Core/Galaxy.cs
@@ -5,5 +5,6 @@ public class Galaxy
     public int id;
     public List<int> neighborIds = new List<int>();
     public int ownerPlayerId = -1;
+    public bool haveSun = true;
     public bool isAlive = true;
 }
