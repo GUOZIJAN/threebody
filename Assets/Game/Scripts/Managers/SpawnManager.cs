56cf2f2d91d801a492d65d57080dca9e761e05c7
GUOZIJIAN <guozijian8848@163.com>
2026-05-05 23:35:37 +0800

    完善卡牌生成逻辑

--- Assets/Game/Scripts/Managers/SpawnManager.cs
+++ Assets/Game/Scripts/Managers/SpawnManager.cs
@@ -4,12 +4,27 @@ using System.Collections.Generic;
 public class SpawnManager : MonoBehaviour
 {
     public static SpawnManager Instance;
-    public Transform[] cardSpawnPoints;
+    public Transform deckPos;
+    public List<Transform> handPoints;
+    public List<Transform> emptyHandPoints;
+    public GameObject cardPrefab;
+    public GameObject buildPrefab;
 
     private void Awake()
     {
         Instance = this;
+        emptyHandPoints = new List<Transform>(handPoints);
     }
 
+    public void SpawnCard()
+    {
+        if (emptyHandPoints.Count > 0)
+        {
+            Transform handPoint = emptyHandPoints[0];
+            emptyHandPoints.RemoveAt(0);
+            GameObject newCard = Instantiate(cardPrefab, deckPos.position, deckPos.rotation);
+            newCard.GetComponent<CardView>().FlyToHand(deckPos.position,handPoint.position);
+        }
+    }
 }
 
